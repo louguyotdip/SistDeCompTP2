0f625adac8e6b8bb2646c1fa883af90d7a523f5c
Gaston Segura <gastonsegura2908@gmail.com>
2024-04-05 10:42:34 -0300

    working with arrays

--- MainProcess.py
+++ MainProcess.py
@@ -1,4 +1,5 @@
 import ctypes
+#import os
 
 import requests
 api_url = "https://jsonplaceholder.typicode.com/todos/1"
@@ -8,14 +9,64 @@ print(response.json())
 #creamos una biblioteca de objetos. Dynamic Link Library(DLL)
 clibrary = ctypes.CDLL("/home/gaston/Documentos/SdC_Proyectos/SistDeCompTP2/ctypes/clibrary.so")
 
-#convertimos la cadena string en una secuencia binaria
+#convertimos la cadena string en una secuencia binaria(en un objeto de bytes)
 #clibrary.display(b"gaston",23)
 
 #asiganamos nombre a la funcion
 func= clibrary.display
 
-#argtypes tiene los tipos argumentos. es una lista de argumentos, ya q pueden haber varios
+#argtypes tiene los tipos argumentos que son los q necesita la funcion func. es una lista de argumentos, ya q pueden haber varios
 func.argtypes = [ctypes.c_char_p,ctypes.c_int]
-#definimos el tipo de retorno de la funcion. solo puede un tipo de retorno,por lo tanto no ponemos entre [](osea una lista)
+#definimos el tipo de retorno de la funcion. solo puede un tipo de retorno,por lo tanto no ponemos entre [](osea una lista). char_p: puntero caracter
 func.restype = ctypes.c_char_p
-clibrary.display(b"gaston",23)
+
+#creamos un buffer para alli almacenar la informacion
+string = ctypes.create_string_buffer(100)
+string.value=b"gaston"
+
+clibrary.display(string,23)
+
+####
+#asiganamos nombre a la funcion
+alloc_func=clibrary.alloc_memory
+#definimos el tipo de retorno de la funcion. es un puntero. osea decimos que queremos un PUNTERO de la clase char
+alloc_func.restype=ctypes.POINTER(ctypes.c_char_p)
+
+#asiganamos nombre a la otra funcion
+free_func=clibrary.free_memory
+##definimos el argumento de la funcion
+free_func.argtypes=[ctypes.POINTER(ctypes.c_char_p)]
+
+cstring_pointer = alloc_func()
+cstring = ctypes.c_char_p.from_buffer(cstring_pointer)
+print(cstring.value)
+free_func(cstring_pointer)
+
+## para crear un puntero en python:
+#aca lo creamos, del tipo int
+ptr2 = ctypes.POINTER(ctypes.c_int)
+#aca ponemos a que apunta
+num=ctypes.c_int(100)
+ptr2.contents=num
+print(ptr2.contents)
+
+
+##obtenemos el directorio de trabajo actual
+#path = os.getcwd()
+#clibrary = ctypes.CDLL(os.path.join(path,'clibrary.so'))
+###creamos una matriz y se la vamos a pasar a C,luego C nos va a devolver una suma q va a hacer con esa matriz
+##creamos la matriz
+values = (ctypes.c_int*10)()
+for i in range(len(values)):
+    values[i]=i
+
+#le pasamos los argumentos a la funcion de c
+sum=clibrary.sumArray(values,len(values))
+print("suma: ",sum)
+
+##ahora al reves, creamos una matriz en C y se la pasamos a python
+#espeficamos el retorno de la funcion de c que vamos a utilizar,que nos va a mandar un array
+clibrary.incArray.restype = ctypes.POINTER(ctypes.c_int)
+result=clibrary.incArray(values,len(values))
+for i in range(10):
+    print(result[i])
--- ctypes/clibrary.c
+++ ctypes/clibrary.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 char* display(char *str, int age){
     printf("Mi nombre es %s y mi edad es %d",str,age);
     return "completado";
+}
+
+char* alloc_memory(){
+    //strdup es como malloc
+    char* str=strdup("hola mundoo");
+    printf("memoria alocada");
+    return str;
+}
+void free_memory(char* ptr){
+    printf("memoria desalcoada");
+    free(ptr);
+}
+
+
+//matriz que recibe desde python
+//le pasamos un puntero al array 
+int sumArray(int *arr,int size){
+    int sum =0;
+    for(int i=0;i<size;i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int* incArray(int *arr,int size){
+    for(int i=0;i<size;i++){
+        arr[i]++;
+    }
+    return arr;
 }
