d8d4f5182fc59485af4842a9978c59d585472a3c
Gaston Segura <gastonsegura2908@gmail.com>
2024-04-05 00:48:05 -0300

    add connection between py & c

--- MainProcess.py
+++ MainProcess.py
@@ -1,4 +1,11 @@
-import requests
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response=requests.get(api_url)
-print(response.json())
+import ctypes
+
+#import requests
+#api_url = "https://jsonplaceholder.typicode.com/todos/1"
+#response=requests.get(api_url)
+#print(response.json())
+
+#creamos una biblioteca de objetos. Dynamic Link Library(DLL)
+clibrary = ctypes.CDLL("/home/gaston/Documentos/SdC_Proyectos/SistDeCompTP2/ctypes/clibrary.so")
+
+clibrary.display()
