f336f7cb59058e6cf94339b555f1e0da8d0f578d
Gaston Segura <gastonsegura2908@gmail.com>
2024-04-06 10:55:47 -0300

    add get data from c function

--- MainProcess.py
+++ MainProcess.py
@@ -1,9 +1,10 @@
 import requests
+import ctypes
 
-#url de la api
+# url de la api
 api_url = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22"
 
-#obtenemos TODOS los datos de la api
+# obtiene TODOS los datos de la api
 response=requests.get(api_url)
 
 # Convierte la respuesta en un diccionario de Python
@@ -25,3 +26,25 @@ for item in data[1]:  # data[1] contiene los datos reales, data[0] contiene meta
 # Imprime los valores
 print(valores)
 
+# crea una biblioteca de objetos. Dynamic Link Library(DLL)
+clibrary = ctypes.CDLL("/home/gaston/Documentos/SdC_Proyectos/SistDeCompTP2/ctypes/clibrary.so")
+
+# asigana nombre a la funcion
+func = clibrary.ChangesArray
+
+# Define el tipo de los argumentos y el valor de retorno de la función
+func.argtypes = [ctypes.POINTER(ctypes.c_float), ctypes.c_int]
+func.restype = ctypes.POINTER(ctypes.c_int)
+
+# Crea un array de C de valores float a partir de la lista de Python
+valores_c = (ctypes.c_float * len(valores))(*valores)
+
+# Llama a la función de c. nuevosvalores_c es un puntero a la memoria que fue asignada en la función ChangesArray del código de C para almacenar el nuevo array de ints.
+nuevosvalores_c = func(valores_c, len(valores))
+
+# Convierte el resultado a una lista de Python(un objeto python)
+valores_po = nuevosvalores_c[:len(valores)]
+print(valores_po) 
+
+#liberar memoria. llama a funcion de programa en c.
+clibrary.free_memory(nuevosvalores_c)
--- ctypes/clibrary.c
+++ ctypes/clibrary.c
@@ -2,3 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+    Esta funcion transforma de tipo float a tipo int y le suma 1 
+    a cada elemento de la array proveniente del codigo en python 
+    @param arr :arreglo con los indices gini
+    @param size :tamaño del arreglo
+*/
+int* ChangesArray(float *arr,int size){
+    int* array_int = malloc(size * sizeof(int));
+    for(int i=0;i<size;i++){
+        array_int[i]=(int)arr[i];
+        array_int[i]++;
+    }
+    return array_int;
+}
+
+void free_memory(int *arr){
+    free(arr);
+}
